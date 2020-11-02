609130e26639043482f6f96acbfaaf77f4fc9b85
JakeMal-AGGP <62573128+JakeMal-AGGP@users.noreply.github.com>
2020-11-01 21:49:33 -0500

    lab 2

---
+++ Assets/Scripts/DropPointScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DropPointScript : MonoBehaviour
+{
+
+    public Material red;
+    public Material green;
+    private Renderer rend;
+
+    void Start()
+    {
+        rend = gameObject.GetComponent<Renderer>();
+        rend.material = red;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        
+        if(other.gameObject.name == "InteractableSphere")
+        {
+
+            rend.material = green;
+
+        }
+
+    }
+}
